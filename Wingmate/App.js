7afeb9737712e793bef3ddf428031ba0c5374bc9
pauljiang03 <jiang861@purdue.edu>
2023-05-18 19:35:24 -0400

    test
    added type

--- Wingmate/App.js
+++ Wingmate/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Monkey</Text>
+      <Text>Monkeys yall</Text>
       <StatusBar style="auto" />
     </View>
   );
