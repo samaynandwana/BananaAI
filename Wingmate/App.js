7411b30ebe8cd10b7ea51455484d63705e9847d9
kambhani <63814718+kambhani@users.noreply.github.com>
2023-05-19 19:07:59 -0400

    Update App.js

--- Wingmate/App.js
+++ Wingmate/App.js
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, Pressable, View } from 'react-native';
+import tw from 'twrnc'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Monkeys yall</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={tw`bg-gray-800 h-full`}>
+      <View style={tw`h-2/5 flex justify-center items-center`}>
+        <Text style={tw`text-5xl text-red-400`}>Wingmate</Text>
+      </View>
+      <View style={tw`h-3/5 flex justify-center items-center`}>
+        <View>
+          <Pressable style={tw`rounded-full bg-blue-400 py-4 w-64 mb-8`}>
+            <Text style={tw`text-center`}>Login</Text>
+          </Pressable>
+          <Pressable style={tw`rounded-full bg-blue-400 py-4 w-64`}>
+            <Text style={tw`text-center`}>Register</Text>
+          </Pressable>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
