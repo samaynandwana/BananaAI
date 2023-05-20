0f07e8a204297e8ee1fe2a66af6c12b3cbc3c8ef
kambhani <63814718+kambhani@users.noreply.github.com>
2023-05-20 19:04:00 -0400

    Add features
    Dark mode and config file

--- Wingmate/App.js
+++ Wingmate/App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, SafeAreaView, Pressable, View } from 'react-native';
-import tw from 'twrnc'
+import { Text, SafeAreaView, Pressable, View } from 'react-native';
+import { useDeviceContext } from 'twrnc';
+import tw from 'twrnc';
+
+//import tw from './lib/tailwind';
 
 export default function App() {
+  useDeviceContext(tw);
   return (
-    <SafeAreaView style={tw`bg-gray-800 h-full`}>
+    <SafeAreaView style={tw`bg-slate-100 dark:bg-slate-800 h-full`}>
       <View style={tw`h-2/5 flex justify-center items-center`}>
-        <Text style={tw`text-5xl text-red-400`}>Wingmate</Text>
+        <View style={tw`flex flex-row`}> 
+          <Text style={tw`text-5xl font-bold text-red-800 dark:text-red-500`}>Wing</Text>
+          <Text style={tw`text-5xl text-red-800 dark:text-red-500`}>mate</Text>
+        </View>
       </View>
       <View style={tw`h-3/5 flex justify-center items-center`}>
         <View>
-          <Pressable style={tw`rounded-full bg-blue-400 py-4 w-64 mb-8`}>
-            <Text style={tw`text-center`}>Login</Text>
+          <Pressable style={tw`rounded-full bg-emerald-700 dark:bg-emerald-500 py-3 w-64 mb-8`}>
+            <Text style={tw`text-center font-semibold text-lg text-slate-100`}>Login</Text>
           </Pressable>
-          <Pressable style={tw`rounded-full bg-blue-400 py-4 w-64`}>
-            <Text style={tw`text-center`}>Register</Text>
+          <Pressable style={tw`rounded-full bg-emerald-700 dark:bg-emerald-500 py-3 w-64`}>
+            <Text style={tw`text-center font-semibold text-lg text-slate-100`}>Register</Text>
           </Pressable>
         </View>
       </View>
---
+++ Wingmate/lib/tailwind.js
@@ -0,0 +1,3 @@
+import { create } from 'twrnc';
+const tw = create(require('../tailwind.config.cjs'));
+export default tw;
