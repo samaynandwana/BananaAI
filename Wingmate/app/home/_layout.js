6b55b3806115caf14040266585020e3a569b9618
kambhani <63814718+kambhani@users.noreply.github.com>
2023-05-22 19:03:40 -0400

    Fix Navigation pt 2

--- Wingmate/app/home/_layout.js
+++ Wingmate/app/home/_layout.js
@@ -1,10 +1,10 @@
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { Stack, Tabs } from "expo-router";
+import { Stack, Tabs, useNavigation } from "expo-router";
 
 export default function HomeLayout() {
   return (
     <SafeAreaProvider>
-      <Stack.Screen options={{headerShown: false}}/>
+      <Stack.Screen options={{headerShown: false, gestureEnabled: false}}/>
       <Tabs>
         <Tabs.Screen name="index" options={{tabBarLabel: "Home", headerShown: false}}/>
         <Tabs.Screen name="nav" options={{tabBarLabel: "Navigation", headerShown: false}}/>
