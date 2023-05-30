73f250950dd00e594bfb6cfaa523e11803426540
pauljiang03 <jiang861@purdue.edu>
2023-05-30 19:51:41 -0400

    auth wrap

--- Wingmate/app/(protected)/clubs/club-landing.js
+++ Wingmate/app/(protected)/clubs/club-landing.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View, Text, StyleSheet, SafeAreaView, Image } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import { withAuthenticator } from "aws-amplify-react-native";
+import theme from '../../../src/auth-theme';
 
 const Clubs = () => {
   return (
@@ -54,8 +56,7 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Clubs;
-
+export default withAuthenticator(Clubs, {theme: theme});
 
 
 
