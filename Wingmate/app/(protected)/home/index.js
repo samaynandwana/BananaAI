fda230d30dcdffa093fc6d8969903e22069fdd96
pauljiang03 <jiang861@purdue.edu>
2023-05-29 21:04:54 -0400

    added to home page
    buttons don't work

--- Wingmate/app/(protected)/home/index.js
+++ Wingmate/app/(protected)/home/index.js
@@ -11,15 +11,62 @@ function Home() {
   return (
     <SafeAreaView>
       <Text
+        style={styleTwo.createBackText}
         onPress={() => {
           router.push('/sign-out');
           Auth.signOut({global: true});
         }}
       >
-        In home dir, press this to sign out
+        Signout
+      </Text>
+      <Text
+        style={styleOne.createCreateText}
+        onPress={() => {
+          router.push('/create')}
+        }
+      >
+        Create Club
+      </Text>
+      <Text
+        style={styleThree.createJoinText}
+        onPress={() => {
+          router.push('/join')}
+        }
+      >
+        Join Club
       </Text>
     </SafeAreaView>
   );
 }
-
 export default withAuthenticator(Home, {theme: theme});
+
+const styleOne = {
+  createCreateText: {
+    fontSize: 25,
+    color: '#8b0000',
+    marginTop: 30,
+  },
+};
+
+const styleTwo = {
+    createBackText: {
+      fontSize: 20,
+      color: 'black',
+      marginTop: 10
+    }
+}
+
+const styleThree = {
+  createJoinText: {
+    fontSize: 25,
+    color: '#8b0000',
+    marginTop: 0
+  }
+}
+
+
+
+
+
+
+
